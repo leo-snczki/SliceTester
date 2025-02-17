7a1a528ce3e20ad9fa663e854be22eb4934834d1
leo-snczki <Leonardo.Zelenski@Slice.pt>
2025-02-17 14:37:37 +0000

    Added a "Classes" folder, and inside the folder there is the class "Mouse" and the basic logic behind it.

--- SliceTester/Form1.cs
+++ SliceTester/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SliceTester.Classes;
 
 namespace SliceTester
 {
@@ -14,8 +15,22 @@ namespace SliceTester
         public Form1()
         {
             InitializeComponent();
+            // Instancia um objeto da classe Mouse.
+            Mouse mouse = new Mouse();
 
+            // Mover o mouse para uma posição específica.
+            mouse.Move(850, 813);
+
+            // Realizar um clique esquerdo 3 vezes com 8s de delay.
+            mouse.Click(3, 8000, leftClick: true);
+            // Realizar um clique com o botão do meio.
+            mouse.Click(1, 2000);
+
+            // Scroll para baixo.
+            mouse.ScrollVertical(-120);
+
+            // Scroll para o lado.
+            mouse.ScrollHorizontal(-150);
         }
-
     }
 }
